4498fad9b083da4420691de81e06e4dbde5ea068
Markus Eckstein <markus.eckstein@gmx.de>
2026-01-07 22:24:30 +0100

    fix: everything

--- terraform/docker-vm.tf
+++ terraform/docker-vm.tf
@@ -6,8 +6,22 @@ resource "proxmox_virtual_environment_file" "cloud_config" {
   source_raw {
     data = <<EOF
 #cloud-config
+username: ubuntu
+chpasswd: { expire: False }
+ssh_pwauth: true
+packages_update: true
+packages_upgrade: true
+groups:
+  - docker
+system_info:
+  default_user:
+    groups: [docker]
+ssh_authorized_keys:
+  - ${trimspace(local.ssh_public_key)}
 packages:
   - qemu-guest-agent
+  - libgl1 
+  - libegl1
   - docker.io
   - docker-compose
 runcmd:
@@ -27,6 +41,7 @@ resource "proxmox_virtual_environment_download_file" "ubuntu_cloud_image" {
   url          = "https://cloud-images.ubuntu.com/noble/current/noble-server-cloudimg-amd64.img"
 }
 
+
 resource "proxmox_virtual_environment_download_file" "ubuntu_lxc_template" {
   content_type = "vztmpl"
   datastore_id = "local"
@@ -41,13 +56,24 @@ resource "proxmox_virtual_environment_vm" "docker_vm" {
 
   cpu { cores = 4 }
   memory { dedicated = 13096 }
+  vga {
+    #type = "virtio-gl"
+    type = "std"
+    memory = 512
+  }
 
+  operating_system {
+    type = "l26"
+  }
   initialization {
-    user_account {
-      keys = [
-        trimspace(local.ssh_public_key)
-      ]
-    }
+    user_data_file_id = proxmox_virtual_environment_file.cloud_config.id
+    # user_account {
+    #   keys = [
+    #     trimspace(local.ssh_public_key)
+    #   ]
+    #   username = "ubuntu"
+    #   password = "linux"
+    # }
     ip_config {
       ipv4 {
         address = "192.168.178.10/24"
@@ -56,7 +82,10 @@ resource "proxmox_virtual_environment_vm" "docker_vm" {
     }
   }
 
-  network_device { bridge = "vmbr0" }
+  network_device {
+     bridge = "vmbr0"
+     model  = "virtio"
+  }
 
   disk {
     datastore_id = "local-lvm"
